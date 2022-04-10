325dfad3bb01f07c6f8557fb0c4b5954597a675b
josst <josue5325@gmail.com>
2022-04-09 21:53:13 -0600

    agregando distintos perfiles

---
+++ src/main/java/com/jossthouse/HolaMundoCOntroller.java
@@ -0,0 +1,14 @@
+package com.jossthouse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+public class HolaMundoCOntroller {
+    @GetMapping("/hola")
+    public String saludar(){
+        return "Hola Mundo :D";
+    }
+}
