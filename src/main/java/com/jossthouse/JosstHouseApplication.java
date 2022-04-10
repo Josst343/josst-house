8b4df2d1e1c829a5dd088a47ab06fd30bc86f5e6
josst <josue5325@gmail.com>
2022-04-09 21:14:10 -0600

    agregando archvios y comprobando conexion

---
+++ src/main/java/com/jossthouse/JosstHouseApplication.java
@@ -0,0 +1,13 @@
+package com.jossthouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosstHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JosstHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jossthouse/JosstHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jossthouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JosstHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
